7ce868256a382c20d2d8b207a703cad534c7bbb7
Charishma Bollineni <139740665+Charishma608@users.noreply.github.com>
2025-06-19 21:54:30 +0530

    Update index.tsx

--- components/atoms/hero-header/index.tsx
+++ components/atoms/hero-header/index.tsx
@@ -8,26 +8,27 @@ function HeroHeader() {
     <h1 className={css({
       marginTop: 0,
       marginBottom: '90px',
-      fontSize: '76px',
-      lineHeight: 1,
+      fontSize: '56px',
+      marginLeft:'-100px',
+      lineHeight: 1.2,
       fontWeight: 700,
       opacity: 1,
       transform: 'translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)',
       transformStyle: 'preserve-3d',
       [theme.mediaQuery.small]: { fontSize: '54px' },
       [theme.mediaQuery.medium]: { fontSize: '76px' },
-      [theme.mediaQuery.large]: { fontSize: '76px' },
+      [theme.mediaQuery.large]: { fontSize: '50px' },
     })}
     >
-      Watch
+      Smart IT
       <span style={dotStyle}>
         .
       </span>
       <br />
-      Learn
+      Reliable Service
       <span style={dotStyle}>.</span>
       <br />
-      Grow
+      Always on Support
       <span style={dotStyle}>.</span>
       <br />
     </h1>
