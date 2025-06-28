e4e9383c9c70888ecfc5f30fc3cacaed0de1a9b8
Charishma Bollineni <139740665+Charishma608@users.noreply.github.com>
2025-06-28 21:25:45 +0530

    Update category-type.tsx

--- lib/category-type.tsx
+++ lib/category-type.tsx
@@ -7,15 +7,15 @@ export type CategoryType = {
 
 export const CATEGORIES: CategoryType[] = [
   {
-    id: 0, name: 'Sales Marketing', duration: '4 months', imageUrl: 'categories_01.jpg',
+    id: 0, name: 'Gen AI', duration: 'Add an X-Factor to your product', imageUrl: 'categories_01.jpg',
   },
   {
-    id: 1, name: 'Data Analytics', duration: '3 months', imageUrl: 'categories_02.jpg',
+    id: 1, name: 'Cloud', duration: 'AWS, GCP, Azure and other', imageUrl: 'categories_02.jpg',
   },
   {
-    id: 2, name: 'Copyrighting Pro', duration: '2 months', imageUrl: 'categories_03.jpg',
+    id: 2, name: 'MVP', duration: 'Test your idea in 3 months', imageUrl: 'categories_03.jpg',
   },
   {
-    id: 3, name: 'Design Art', duration: '4 months', imageUrl: 'categories_04.jpg',
+    id: 3, name: 'Staff Augmentation', duration: 'Hire talented Candidates', imageUrl: 'categories_04.jpg',
   },
 ];
